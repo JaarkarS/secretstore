f8d63a231b091faace2b5cea9c8938d87eb514a8
Jay Krishna Korrapati <78617436+JaarkarS@users.noreply.github.com>
2024-05-04 07:19:12 -0400

    Update passmanager.py

--- passmanager.py
+++ passmanager.py
@@ -1,31 +1,39 @@
+from cryptography.fernet import Fernet
 import hashlib
-import getpass
 
-password_manager = {}
 
-def create_account():
-    username = input("enter user:"); 
-    password = getpass.getpass("enter passcode");
-    hash_password = hashlib.sha256(password.encode()).hexdigest()
-    password_manager[username] = hash_password
-    print("account created success")
+def create_secret():
+    keystring = input("enter key:"); 
+    secr3t = getpass.getpass("enter secr3t:");
+    print("original string: ", secr3t)
+    hash_secr3t = fernet.encrypt(secr3t.encode("utf-8"))                                                             
+    print("encrypted string: ", hash_secr3t)
+    key_secr3t_manager[keystring] = hash_secr3t
+    with open('keysecr3t.json', 'w', encoding='utf-8') as fp:
+     json.dump(key_secr3t_manager, fp)
+    print("secret stored hapyly")
 
-def login():
-     username = input("enter user :")    
-     password = getpass.getpass("enter your password: ")
-     hash_password = hashlib.sha256(password.encode()).hexdigest()
-     if username in password_manager.keys() and password_manager[username] == hash_password:
-          print("login successfull")
-     else:
-          print("invalid username or password")
+def get_scret():
+
+     keystring = input("enter key :")    
+     with open('keysecr3t.json', 'r') as fp:
+         key_secr3t_manager = json.load(fp)
+     for key in key_secr3t_manager:
+           if(key == keystring):
+            value=key_secr3t_manager[keystring]
+            value=fernet.decrypt(value)
+            #.decode("utf-8")
+            print (value)
+            break  
+     #hash_secr3t = hashlib.sha256(secr3t.encode()).hexdigest()
 
 def main():
      while True:
-          choice = input("enter 1 to create an account, 2 to login, or 0 to exit: ")
+          choice = input("enter 1 to create a secret, 2 to get existing secret, or 0 to exit: ")
           if choice == "1":
-               create_account()
+               create_secret()
           elif choice == "2":
-               login()
+               get_scret()
           elif choice == "0":
                break
           else:  
@@ -33,5 +41,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-                    
-     
